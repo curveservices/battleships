63d5e69439568f66ee2fdc3b65a559ee37f58353
Paul <p.rossiter83@gmail.noreply.github.com>
2023-11-21 13:20:46 +0000

    Logic added for valid blocks to place random ships

--- src/app.js
+++ src/app.js
@@ -60,7 +60,7 @@ const app = () => {
             randomStartIndex <= width * width - width * ship.length ? randomStartIndex : 
                 randomStartIndex - ship.length * width + width
 
-        let shipBlocks = []
+        let shipBlocks = [];
 
         for (let i = 0; i < ship.length; i++) {
             if (isHorizontal) {
@@ -68,12 +68,28 @@ const app = () => {
             } else {
                 shipBlocks.push(allBoardBlocks[Number(validStart) + i * width])
             }
+        };
+        let valid;
+
+        if (isHorizontal) {
+        shipBlocks.every((_shipBlock, index) =>
+           valid = shipBlocks[0].id % width !== width - (shipBlocks.length - (index + 1)))
+        } else {
+            shipBlocks.every((_shipBlock, index) =>
+                valid = shipBlocks[0].id < 90 + (width * index + 1))
         }
-        shipBlocks.forEach(shipBlock => {
-            shipBlock.classList.add(ship.name)
-            shipBlock.classList.add('taken')
-        })
-    }
+
+        const notTaken = shipBlocks.every(shipBlock => !shipBlock.classList.contains('taken'))
+
+        if (valid && notTaken) {
+            shipBlocks.forEach(shipBlock => {
+                shipBlock.classList.add(ship.name)
+                shipBlock.classList.add('taken')
+            })
+        } else {
+            addShipPiece(ship)
+        };
+    };
 
     ships.forEach(ship => addShipPiece(ship))
 
